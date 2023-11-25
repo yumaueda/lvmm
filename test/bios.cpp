935eaa813d965e7b5ee22daa3e16c53fcc746919
Yuma Ueda <yumaueda@proton.me>
2023-11-26 01:42:24 +0900

    Introduced the test fixture

--- test/bios.cpp
+++ test/bios.cpp
@@ -3,25 +3,37 @@
 
 namespace {
 
-TEST(MpGenChecksum, HandleMpFps) {
-    mpfps fps;
-    fps.phys_addr_ptr = EBDA_START+0x40;
-    ASSERT_EQ(0x5b, mp_gen_checksum(&fps));
+class MpFpsTest : public testing::Test {
+    protected:
+        mpfps fps_default_state;
+        mpfps fps_with_checksum;
+        static constexpr uint8_t VALID_CHECKSUM = 0x5b;
+
+        virtual void SetUp() {
+            fps_default_state.phys_addr_ptr = EBDA_START+0x40;
+            fps_with_checksum.checksum = mp_gen_checksum(&fps_with_checksum);
+        }
+};
+
+/*
+class MpCtableTest : public testing::Test {
+    protected:
+        mpctable
+};
+*/
+
+TEST_F(MpFpsTest, CalcChecksum) {
+    ASSERT_TRUE(mp_calc_checksum(&fps_default_state));
+    ASSERT_FALSE(mp_calc_checksum(&fps_with_checksum));
 }
 
-TEST(MpCalcChecksum, HandleMpFps) {
-    mpfps fps;
-    fps.phys_addr_ptr = EBDA_START+0x40;
-    fps.checksum = 0x5b;
-    ASSERT_EQ(0x00, mp_calc_checksum(&fps));
+TEST_F(MpFpsTest, GenChecksum) {
+    ASSERT_EQ(VALID_CHECKSUM, mp_gen_checksum(&fps_default_state));
 }
 
-TEST(IsMpChecksumValid, HandleMpFps) {
-    mpfps fps;
-    fps.phys_addr_ptr = EBDA_START+0x40;
-    fps.checksum = 0x5b;
-    ASSERT_TRUE(is_mp_checksum_valid(&fps));
+TEST_F(MpFpsTest, IsMpChecksumValid) {
+    ASSERT_FALSE(is_mp_checksum_valid(&fps_default_state));
+    ASSERT_TRUE(is_mp_checksum_valid(&fps_with_checksum));
 }
-
 
 }  // namespace
